7890455cf5bbe25a05cf3aba61ba5cc55535d86a
Noman Waseem <nomanwaseem111@gmail.com>
2022-12-09 04:01:29 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 import './components/index.css'
-import Offer from './components/Offer';
+
+
 import {
   Routes,
   Route,
-  
+
 } from "react-router-dom";
-import Home from './components/Home'
 import Accessories from './components/Accessories'
 import Mobile from './components/Mobile'
 import Contact from './components/Contact'
 import Banner from './components/Banner'
-import Deals from './components/Deals';
+import Deals from './components/Deals'
+import Offer from './components/Offer'
 import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/accessories" element={<Accessories />} />
-      <Route path="/mobile" element={<Mobile />} />
-      <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/mobile" element={<Mobile />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Banner/>
-      <Offer/>
-      <Deals/>
-      <Footer/>
+      <Home />
+      <Banner />
+
+      <Offer />
+      <Deals />
+
+      <Footer />
     </>
   )
 }
